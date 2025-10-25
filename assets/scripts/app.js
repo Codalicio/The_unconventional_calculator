baa93f64b73dd52ad09e4963c9b5e3fe921303af
Amit Raj <rajamitnine@gmail.com>
2025-10-25 18:25:12 +0530

    Remove unwanted code

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,15 +5,8 @@ const add = (num1, num2) => {
   return result;
 };
 
-const additionResult = add(4, 3);
-console.log(additionResult);
-
 let currentResult = defaultResult;
 
 currentResult = add(9, 7);
 
-let calculationDescription = `(${defaultResult} + 10)
-
-+ 20 / 2 - 10`;
-
-outputResult(currentResult, calculationDescription);
+outputResult(currentResult, "");
